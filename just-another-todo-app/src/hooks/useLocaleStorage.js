6e75a95761dd3ff288948f96dbf5ba27dc57a580
Hiro Lestrade <hiroshi.lestrade@ingenieria.unam.edu>
2023-08-27 01:06:10 -0600

    refactor: useLocaleStorage

--- just-another-todo-app/src/hooks/useLocaleStorage.js
+++ just-another-todo-app/src/hooks/useLocaleStorage.js
@@ -1,26 +1,15 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 const useLocalStorage = (key, defaultValue) => {
+	const [value, setValue] = useState(
+		JSON.parse(localStorage.getItem(key)) ?? defaultValue
+	);
+
+	useEffect(() => {
+		localStorage.setItem(key, JSON.stringify(value));
+	}, [value, key]);
 	
-	const [localStorageValue, setLocalStorageValue] = useState(() => {
-        const value = localStorage.getItem(key);
-			
-        if (value) {
-            return JSON.parse(value);
-        }
-			
-        else{
-            localStorage.setItem(key, JSON.stringify(defaultValue));
-            return defaultValue;
-        }
-    });
-
-	const setLocalStorageStateValue = (newValue) => {
-		localStorage.setItem(key, JSON.stringify(newValue));
-		setLocalStorageValue(newValue);
-	}
-
-	return [localStorageValue, setLocalStorageStateValue];
+	return [value, setValue];
 };
 
 export default useLocalStorage;
